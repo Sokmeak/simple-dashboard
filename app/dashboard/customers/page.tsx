468aff0d5784a0dfc5016d6c66fccddb5ff31245
MonoMeak <sokmeak.sarenn@gmail.com>
2026-03-31 08:33:49 +0700

    Update customer by wrapping with suspense

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,15 @@
 import { Metadata } from "next";
 import CustomersTable from "@/app/ui/customers/table";
 import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
 export const metadata: Metadata = {
   title: "Customer",
 };
 export default async function Page() {
   const customers = await fetchFilteredCustomers("");
-  return <CustomersTable customers={customers} />;
+  return (
+    <Suspense>
+      <CustomersTable customers={customers} />;
+    </Suspense>
+  );
 }
